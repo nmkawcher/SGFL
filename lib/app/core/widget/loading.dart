926bd8aa51488bdcd98fb3b88d4d90f2028ee148
Parvez <gias.parvez@link3.net>
2024-07-18 01:56:18 +0600

    add icon

--- lib/app/core/widget/loading.dart
+++ lib/app/core/widget/loading.dart
@@ -1,20 +1,34 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
+import '../values/app_images.dart';
 import '/app/core/values/app_colors.dart';
-import '/app/core/values/app_values.dart';
-import '/app/core/widget/elevated_container.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
 
 class Loading extends StatelessWidget {
   const Loading({Key? key}) : super(key: key);
-
+  AppLocalizations? get appLocalization => AppLocalizations.of(Get.context!);
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: ElevatedContainer(
-        padding: EdgeInsets.all(AppValues.margin),
-        child: CircularProgressIndicator(
-          color: AppColors.colorPrimary,
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title:   Text('SGFL',style: const TextStyle(color: Colors.white)),
+        elevation: 0,
+        centerTitle: true,
+        backgroundColor: AppColors.accentPrimary,
+        systemOverlayStyle: const SystemUiOverlayStyle(
+            statusBarColor: Colors.transparent,
+            statusBarIconBrightness: Brightness.light),
+      ),
+      body:  Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(child: Lottie.asset(AppImages.loading)),
+          Text(appLocalization!.gettingEverythingReady,style: const TextStyle(fontSize: 16),)
+        ],
       ),
     );
   }
